843d9ac91fb1f07ca0211e88b598816ec9b21954
DevBilal17 <muhammadbilalmb0312@gmail.com>
2025-07-17 06:05:34 +0500

    Random_Number

---
+++ random_number/script.js
@@ -0,0 +1,18 @@
+
+
+var random = Math.random();
+// console.log(random) // b/w 0 and 1
+
+// For random numbers between 1 and 10
+// var highest = 10;
+// var lowest = 1;
+
+// var a = Math.floor(Math.random()*(highest - lowest) + lowest);
+
+// console.log(a)
+
+
+// for 4 digit otp creation
+
+var otp = Math.floor(Math.random() * 10000);
+console.log(otp)

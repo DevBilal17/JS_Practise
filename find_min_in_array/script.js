8f02becaafd275619b1fab7424e2ae0be60d30cf
DevBilal17 <muhammadbilalmb0312@gmail.com>
2025-07-22 15:40:30 +0500

    Min in Array

---
+++ find_min_in_array/script.js
@@ -0,0 +1,15 @@
+var arr = [1,4,-2,10,34,23,9,0,-100];
+
+
+let min = arr[0];
+
+
+for(let i=1;i<arr.length;i++){
+    if(min > arr[i]){
+       min = arr[i]
+    }
+}
+
+
+
+console.log(`Minimum number in array ${arr} is ${min}`)
